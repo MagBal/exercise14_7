18e2d5ff597867b50c12fefb9380e3a2d6045dfc
Magda Balun <magi@op.pl>
2018-01-09 13:20:19 +0100

    Fix App.js

--- components/App.js
+++ components/App.js
@@ -27,7 +27,7 @@ var contactForm = {
 var App = React.createClass({
     render: function() {
         return (
-            <div className: {'app'}>
+            <div className={'app'}>
                 <ContactForm contact={contactForm}/>
                 <Contacts items={contacts}/>
             </div>
